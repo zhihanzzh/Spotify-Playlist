c2b6e0a2736e568fa0aad77a51dd686971522daf
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-02-27 17:44:14 -0500

    added some fake data

--- src/App.js
+++ src/App.js
@@ -38,9 +38,12 @@ class HoursCounter extends Component {
   render() {
     let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
       return songs.concat(eachPlaylist.songs)},[])
+      let totalDuration = allSongs.reduce((sum, eachSong) => {
+        return sum + eachSong.duration
+      }, 0)
     return (
       <div style={{ width: '40%', display: 'inline-block' }}>
-        <h2>{allSongs.length} Hours</h2>
+        <h2>{totalDuration} Hours</h2>
       </div>
     );
   }
