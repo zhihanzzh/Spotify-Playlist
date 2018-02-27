ac7fde76fec8d325f2c21020e1c23eeddc25168b
zhihanzzh <zzhzhangzhihan@yahoo.com>
2018-02-26 22:01:24 -0500

    added number of playlist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+let fakeServerData = {
+  user: {
+    name: 'Han',
+    playlists: [
+      {
+        name: 'p1',
+        songs: ['s1', 's2', 's3']
+      }, {
+        name: 'p2',
+        songs: ['s21', 's22', 's23']
+      }, {
+        name: 'p3',
+        songs: ['s31', 's32', 's33']
+      }, {
+        name: 'p4',
+        songs: ['s41', 's42', 's43']
+      }
+    ]
+  }
+}
+
 class Aggregate extends Component {
   render() {
     return (
       <div style={{ width: '40%', display: 'inline-block' }}>
-        <h2>Number Text</h2>
+        <h2>{this.props.playlists && this.props.playlists.length} Text</h2>
       </div>
     );
   }
@@ -28,14 +50,14 @@ class Playlist extends Component {
   render() {
     return (
       <div style={{ width: '25%', display: 'inline-block' }}>
-      <img/>
-      <h3>playlist Name</h3>
-      <ul>
-      <li>song1</li>
-      <li>song1</li>
-      <li>song1</li>
-      <li>song1</li>
-      </ul>
+        <img />
+        <h3>playlist Name</h3>
+        <ul>
+          <li>song1</li>
+          <li>song1</li>
+          <li>song1</li>
+          <li>song1</li>
+        </ul>
       </div>
 
     );
@@ -43,11 +65,23 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      serverData: fakeServerData
+    })
+  }
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
-        <Aggregate />
+        <h1>{this.state.serverData.user && this.state.serverData.user.name}'s playlist
+        </h1>
+        <Aggregate playlists={this.state.serverData.user && this.state.serverData.user.playlists}/>
         <Aggregate />
         <Filter />
         <Playlist />
